219eac9e383f9b339214a0ceca123429bce6c7e0
hsmint <hsmint.hong@gmail.com>
2020-08-11 01:54:46 +0900

    📌Token unreveal

--- Bot/main.py
+++ Bot/main.py
@@ -2,7 +2,7 @@ import discord
 import os, random, asyncio, time
 from discord.ext import commands
 
-token = 'NzQyNDEzMjc3ODMyODA2NDQw.XzFwPg.-OlfeEap75uL--CSMMn0TIYC6is'
+token = 'your token'
 
 bot = commands.Bot(command_prefix="!", activity=discord.Activity(name="Whale | !help", type=1))
 
@@ -22,7 +22,7 @@ class Jaram(commands.Cog):
     @commands.command()
     async def admin(self, ctx):
         """자람 임원진을 소개합니다"""
-        message = '```\n자람 임원진을 소개합니다.\n회장 : 유진웅.35\n부회장 : 이승훈.35\n서버관리자 : 강찬영.35\n학술부장 : 홍석민.35\n홍보부장 : 박예빈.35\n섭외부장 : 전상민.35\n회계 : 임종협.35\n```'
+        message = '```\n자람 임원진을 소개합니다.\n회장 : 유진웅.35\n부회장 : 이승훈.35\n서버관리자 : 강찬영.31\n학술부장 : 홍석민.35\n홍보부장 : 박예빈.35\n섭외부장 : 전상민.35\n회계 : 임종협.35\n```'
         await ctx.send(message) 
 
 class Whale(commands.Cog):
@@ -33,7 +33,7 @@ class Whale(commands.Cog):
     async def whale(self, ctx):
         """Show random pictures of whale emoticon"""
         pick = random.randrange(1, 30)
-        await ctx.send(file=discord.File('image/'+str(pick)+'.png'))
+        await ctx.send(file=discord.File('./image/'+str(pick)+'.png'))
 
 #bot Catergory
 bot.add_cog(Whale(bot))
