b0e776c68b1ffa6977041cce90ce25d342a2da36
hsmint <hsmint.hong@gmail.com>
2020-08-18 19:42:11 +0900

    👀 Workshop substitute  to homework

---
+++ Homework/linear_regression/linear_regression.py
@@ -0,0 +1,6 @@
+# TODO part 1. 머신 러닝에 필요한 모듈을 가져온다.
+# TODO part 2. 폴더 안에 있는 boston.csv를 가져온다
+# TODO part 3. boston.csv안에 없는 값들은 열의 평균으로 대체한다.
+# TODO part 4. boston.csv에 값들에 대해서 요약 통계을 구성한다.
+# TODO part 5. 'heatmap’을 활용하여 관계가 높는 값들을 이용해 데이터 셋을 구성한다.
+# TODO part 6. 머신 러닝 알고리즘 중에서 선형 회귀를 이용하여 머신 러닝을 구현한다.
---
+++ Homework/webcrawling/amongus.py
@@ -0,0 +1,11 @@
+# TODO part 1. import module
+
+# TODO part 2. 네이버에 '어몽 어스'대하여 뉴스 검색
+# hint. urllib 와 beautifulsoup 를 이용한다.
+url = "https://search.naver.com/search.naver?where=news&query=" + \
+    "어몽%20어스&sm=tab_opt&sort=0&photo=0&field=0&reporter_article=&pd=3&ds=" + \
+    "2020.08.04&de=2020.08.18"
+# TODO part 3. 검색된 뉴스들에 대하여 제목을 ‘news_title’ 에 저장한다.
+# TODO part 4. 검색된 뉴스들 중에서 제목에 ‘폴 가이즈’를 가진 제목을 found 에 저장한다.
+# TODO part 5. 검색된 뉴스들의 개수와 found을 출력한다.
+
